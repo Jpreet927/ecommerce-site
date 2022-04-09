0605b1a4f937f094577ed956c73de5a945463162
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2022-04-09 09:35:39 -0400

    project arch

--- src/App.js
+++ src/App.js
@@ -2,22 +2,11 @@ import logo from './logo.svg';
 import './Styles/App.css';
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
